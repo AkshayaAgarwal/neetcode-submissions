47446fde6c824bff0bea8c0491864380f2639015
Akshaya Agarwal <117337745+AkshayaAgarwal@users.noreply.github.com>
2026-06-13 22:35:20 +0530

    Add: sql-customers-bought-a-b-not-c - submission-1

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-1.sql
@@ -0,0 +1,11 @@
+select c.customer_id, c.customer_name
+from customers c
+left join orders o 
+on c.customer_id=o.customer_id
+group by c.customer_id, c.customer_name
+having 
+    count(case when o.product_name='A' then 1 end)>0
+    and count(case when o.product_name='B' then 1 end)>0
+    and count(case when o.product_name='C' then 1 end)=0
+order by c.customer_name
+
